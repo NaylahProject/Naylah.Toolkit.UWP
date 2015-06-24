7a0c2bf02f4c50c5f53868c15d88281be51a1841
Breno Santos <angusbreno@hotmail.com>
2015-06-24 01:08:02 -0300

    Setting up AppVeyor CI

---
+++ Naylah.UWP/Naylah.UWP.Tests/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Naylah.UWP.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var s = new Architecture.Sandbox();
+
+            Assert.AreNotEqual(s.N(), s.N2());
+        }
+    }
+}
---
+++ Naylah.UWP/Naylah.UWP/Architecture/Sandbox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naylah.UWP.Architecture
+{
+    public class Sandbox
+    {
+        public int N()
+        {
+            return 0;
+        }
+
+        public int N2()
+        {
+            return 1;
+        }
+    }
+}
